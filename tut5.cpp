3cc6f699eca268f83de8a5a2b9752ea33bc8f14c
Sh-oaib <123928279+Sh-oaib@users.noreply.github.com>
2023-09-19 15:53:13 +0530

    Add files via upload

---
+++ tut5.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+// ********** break **************
+int main(){
+   // int i = 5;
+    // for (int i = 0; i < 4; i++)
+    // {
+    //     /* code */
+     
+    //     if(i==2){
+    //         break;
+    //     }
+    //     cout<<i<<endl;
+    // }
+   // cout<<i<<endl;
+// ***************** Continue **************** 
+    for (int i = 0; i < 4; i++)
+    {
+        /* code */\
+     
+        if(i==2){
+            continue;
+        }
+        cout<<i<<endl;
+    }
+    return 0;
+    
+}
